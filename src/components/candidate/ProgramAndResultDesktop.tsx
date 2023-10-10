e42217850823235c737436d5f772e3e6329baccd
FarragoCommunity <102537372+FarragoCommunity@users.noreply.github.com>
2023-10-10 19:52:30 +0000

    nrew

--- src/components/candidate/ProgramAndResultDesktop.tsx
+++ src/components/candidate/ProgramAndResultDesktop.tsx
@@ -10,7 +10,7 @@ interface Props {
   candidate: Candidate;
 }
 
-const Profile = (props: Props) => {
+export default function ProgramAndResultDesktop(props: Props) {
   const [publishedResults, setPublishedResults] = useState<
     CandidateProgramme[]
   >([]);
@@ -156,7 +156,7 @@ const Profile = (props: Props) => {
             </div>
             <hr className="border" />
             {/* list */}
-            <div className="mx-5 flex flex-col items-center overflow-y-auto gap-5 h-41/50 py-5">
+            <div className="mx-5 flex flex-col items-center overflow-y-auto gap-5 h-4/5 py-5">
               {/* programslist Desktop */}
               {allOrIndividualOrGroup === "all"
                 ? allPrograms?.map((programme) => (
@@ -166,8 +166,8 @@ const Profile = (props: Props) => {
                       </h1>
                       <h1 className="mt-4">{programme?.programme?.name}</h1>
                       <div className="text-lt flex flex-col items-end mt-2">
-                        <p>7:30</p>
-                        <p>19-09-2023</p>
+                        <p>-</p>
+                        <p>-</p>
                       </div>
                     </div>
                   ))
@@ -179,8 +179,8 @@ const Profile = (props: Props) => {
                       </h1>
                       <h1 className="mt-4">{programme?.programme?.name}</h1>
                       <div className="text-lt flex flex-col items-end mt-2">
-                        <p>7:30</p>
-                        <p>19-09-2023</p>
+                        <p>-</p>
+                        <p>-</p>
                       </div>
                     </div>
                   ))
@@ -192,8 +192,8 @@ const Profile = (props: Props) => {
                       </h1>
                       <h1 className="mt-4">{programme?.programme?.name}</h1>
                       <div className="text-lt flex flex-col items-end mt-2">
-                        <p>7:30</p>
-                        <p>19-09-2023</p>
+                        <p>-</p>
+                        <p>-</p>
                       </div>
                     </div>
                   ))
@@ -302,8 +302,16 @@ const Profile = (props: Props) => {
                         <p className="">{programme?.programme?.name}</p>
                       </div>
                       <div className="text-sm flex gap-2 flex-row items-end -pt-2 ml-24">
-                        <p>{programme?.position?.name}</p>
-                        <p>{programme?.grade?.name}</p>
+                        <p>
+                          {programme?.position?.name
+                            ? programme?.position?.name
+                            : `Nil`}
+                        </p>
+                        <p>
+                          {programme?.grade?.name
+                            ? programme?.grade?.name
+                            : `Nil`}
+                        </p>
                         <p>{programme?.point}pts</p>
                       </div>
                     </div>
@@ -524,6 +532,4 @@ const Profile = (props: Props) => {
       </div>
     </>
   );
-};
-
-export default Profile;
+}
